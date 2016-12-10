4e30f30ce369af9e14bea89e8e751b3bef9fef70
irok <t.irokawa@gmail.com>
2016-12-11 07:49:20 +0900

    update example

--- example/importer.js
+++ example/importer.js
@@ -3,15 +3,8 @@
 var configure = require('../');
 
 module.exports = configure({
-    debug: true,
     images_dir:                 'example/images',
     generated_images_dir:       'example/htdocs/images',
     http_stylesheets_path:      'css',
-    http_generated_images_path: 'images',
-
-    vars: {
-        '$default-sprite-separator': '_',
-        '$icons-sprite-dimensions': true,
-        '$icons-class-separator': '-'
-    }
+    http_generated_images_path: 'images'
 });
