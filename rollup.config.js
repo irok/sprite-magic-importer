e2d2a5287c30fa18712d977f191003c576c00d5d
irok <t.irokawa@gmail.com>
2019-07-17 13:54:35 +0900

    update modules

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,17 @@ import json from 'rollup-plugin-json';
 const { dependencies } = require('./package.json');
 
 export default {
-    entry: 'src/index.js',
-    dest:  'lib/index.js',
-    format: 'cjs',
+    input: 'src/index.js',
+    output: {
+        file: 'lib/index.js',
+        format: 'cjs'
+    },
     plugins: [
         json(),
-        babel()
+        babel({
+            babelrc: false,
+            presets: [['@babel/env', { modules: false }]]
+        })
     ],
     external: Object.keys(dependencies).concat([
         'os', 'path', 'crypto'
